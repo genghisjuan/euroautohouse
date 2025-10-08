dc6e1fd5edb70b5b932af2d5c39c6e481f1ae1c4
Genghis Juan <112909783+genghisjuan@users.noreply.github.com>
2025-10-07 23:04:14 -0400

    Update page.jsx

--- app/(site)/services/page.jsx
+++ app/(site)/services/page.jsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Section from "@/components/Section";
 import Link from "next/link";
 import { SITE } from "@/config/site.config";
